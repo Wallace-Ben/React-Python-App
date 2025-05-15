72132366caf9e4e577922fe33fdf4e063c67af96
Wallace-Ben <125564099+Wallace-Ben@users.noreply.github.com>
2025-05-15 15:12:54 +0100

    First API call

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+const UNSPLASH_BASE_URL = `https://api.unsplash.com/photos/random/`;
+
 function App() {
   const [searchTerm, updateSearchTerm] = useState("");
+  const queryString = `?query=${searchTerm}&client_id=${UNSPLASH_KEY}`;
 
-  function searchImageHandler(e) {
+  async function searchImageHandler(e) {
     e.preventDefault();
     console.log(searchTerm);
+
+    try {
+      const response = await fetch(`${UNSPLASH_BASE_URL}${queryString}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
+
   return (
     <div className="App">
       <Header title="Images Gallery" />
