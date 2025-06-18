198037c776ad2914c1f680159492872513172e3d
Wallace-Ben <125564099+Wallace-Ben@users.noreply.github.com>
2025-06-18 09:21:12 +0100

    Adding request to unsplash API via the backend

--- images-gallery/api/main.py
+++ images-gallery/api/main.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="./.env.local")
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random/'
+UNSPLASH_KEY = os.environ.get("UNSPLASH_API_KEY", "")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please create .env.local file and ensure you have an UNSPLASH_API_KEY")
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+    # Retrieve query parameters from client request
+    word = request.args.get("query")
+
+    headers = {
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
+        "Accept-Version": "v1"
+    }
+
+    params = {
+        "query": word
+    }
+
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+
+    data = response.json()
+
+    return data
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5050, debug=True)
