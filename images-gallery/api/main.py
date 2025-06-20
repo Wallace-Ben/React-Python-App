23ddd2a0543cd96053b0bacc85e4d6dfd6b1c5ff
Wallace-Ben <125564099+Wallace-Ben@users.noreply.github.com>
2025-06-20 09:54:36 +0100

    Enabling togglable debug mode

--- images-gallery/api/main.py
+++ images-gallery/api/main.py
@@ -6,11 +6,13 @@ import os
 load_dotenv(dotenv_path="./.env.local")
 UNSPLASH_URL = 'https://api.unsplash.com/photos/random/'
 UNSPLASH_KEY = os.environ.get("UNSPLASH_API_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and ensure you have an UNSPLASH_API_KEY")
 
 app = Flask(__name__)
+app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
@@ -33,4 +35,4 @@ def new_image():
     return data
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5050, debug=True)
+    app.run(host="0.0.0.0", port=5050)
