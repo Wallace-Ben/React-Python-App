f5e9d9b7b37eaf42baa0285589321d88e68e5369
Wallace-Ben <125564099+Wallace-Ben@users.noreply.github.com>
2025-06-25 09:24:45 +0100

    Enabling CORS

--- images-gallery/api/main.py
+++ images-gallery/api/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import requests
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 UNSPLASH_URL = 'https://api.unsplash.com/photos/random/'
@@ -12,6 +13,7 @@ if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and ensure you have an UNSPLASH_API_KEY")
 
 app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
