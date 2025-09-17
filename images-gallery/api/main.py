572d1d1143ea3ab70a222a3bd34dfc0543483f24
Wallace-Ben <125564099+Wallace-Ben@users.noreply.github.com>
2025-09-17 09:00:04 +0100

    Basic images endpoint with get and post methods

--- images-gallery/api/main.py
+++ images-gallery/api/main.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 from dotenv import load_dotenv
 from flask_cors import CORS
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -19,20 +23,34 @@ app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
     # Retrieve query parameters from client request
     word = request.args.get("query")
-
     headers = {"Authorization": "Client-ID " + UNSPLASH_KEY, "Accept-Version": "v1"}
-
     params = {"query": word}
-
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-
     data = response.json()
 
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        # read images from the database
+        images = images_collection.find({})
+        result = []
+        for img in images:
+            img["id"] = str(img["_id"])  # This is important!
+            result.append(img)
+        return jsonify(result)
+    if request.method == "POST":
+        # save image to the database
+        image = request.get_json()
+        result = images_collection.insert_one(image)
+        return jsonify({"id": str(result.inserted_id)}), 201
 
 
 if __name__ == "__main__":
